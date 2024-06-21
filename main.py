4cf1c4257ca80ad7a556029c0bbe39ad0d4e429b
Red Magnos <redmagnos@gmail.com>
2024-06-20 21:18:27 -0300

    Create create_bridge function
    Create global variables outbound_bridges and inbound_bridges and create a function to populate them

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from bridge import Bridges
 import discord
 import json
+from typing import cast
 
 # Create the client connection
 intents = discord.Intents()
@@ -15,6 +17,9 @@ client = discord.Client(intents=intents)
 credentials = json.load(open("credentials.json"))
 
 is_ready = False  # whether the bot is ready and doesn't need to be readied again
+outbound_bridges: dict[int, Bridges] = {}
+inbound_bridges: dict[int, dict[int, Bridges]] = {}
+
 
 @client.event
 async def on_ready():
@@ -32,6 +37,31 @@ async def on_ready():
 @client.event
 async def on_message(message: discord.Message):
     print(message.content)
+
+
+async def create_bridge(
+    source: discord.TextChannel | discord.Thread | int,
+    target: discord.TextChannel | discord.Thread | int,
+    webhook: discord.Webhook | None = None,
+):
+    global outbound_bridges, inbound_bridges
+
+    if isinstance(source, int):
+        source_id = source
+    else:
+        source_id = source.id
+
+    if isinstance(target, int):
+        target = cast(discord.TextChannel | discord.Thread, client.get_channel(target))
+    assert isinstance(target, discord.TextChannel | discord.Thread)
+
+    if not outbound_bridges.get(source_id):
+        outbound_bridges[source_id] = Bridges(source_id)
+    await outbound_bridges[source_id].add_target(target, webhook)
+
+    if not inbound_bridges.get(target.id):
+        inbound_bridges[target.id]
+    inbound_bridges[target.id][source_id] = outbound_bridges[source_id]
 
 
 client.run(credentials["app_token"], reconnect=True)
