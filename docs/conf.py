051ca17f4aa3fd548badae95446448530f1ddfeb
Red Magnos <redmagnos@gmail.com>
2024-07-23 14:42:00 -0300

    Make documentation dark mode

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,8 @@ release = version
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx_rtd_dark_mode"]
+default_dark_mode = True
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
