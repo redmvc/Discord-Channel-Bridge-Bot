b1f8eb6bf5ae3b17c0f86ddc93d9799a7a2cd392
Red Magnos <redmagnos@gmail.com>
2025-07-03 10:47:34 -0300

    Create function to abstract client connection

--- src/main.py
+++ src/main.py
@@ -2678,7 +2678,49 @@ async def bridge_unbridged_messages():
                     break
 
 
-app_token = globals.settings.get("app_token")
-assert isinstance(app_token, str)
-logger.info("Connecting client...")
-globals.client.run(app_token, reconnect=True)
+@overload
+def start_client() -> None:
+    """Start the client and connect to Discord. This function is blocking."""
+    ...
+
+
+@overload
+def start_client(blocking: Literal[True]) -> None:
+    """Start the client and connect to Discord. This function is blocking."""
+    ...
+
+
+@overload
+def start_client(blocking: Literal[False]) -> Coroutine[Any, Any, None]:
+    """Return a Coroutine that can be awaited or passed to an asyncio event loop which starts the bot client and connects to Discord without blocking execution.
+
+    Returns
+    -------
+    Coroutine[Any, Any, None]
+    """
+    ...
+
+
+def start_client(blocking: bool = True) -> Coroutine[Any, Any, None] | None:
+    """Start the client and connect to Discord. If `blocking` is set to False, this function will instead return a Coroutine that can be awaited or passed to an asyncio event loop with a non-blocking connection.
+
+    Parameters
+    ----------
+    blocking : bool, optional
+        Whether to run the blocking version of the connection. If set to False, this function will return a non-blocking Coroutine to connect to the servers. Defaults to True.
+
+    Returns
+    -------
+    Coroutine[Any, Any, None] | None
+    """
+    app_token = globals.settings.get("app_token")
+    assert isinstance(app_token, str)
+    logger.info("Connecting client...")
+    if blocking:
+        globals.client.run(app_token, reconnect=True)
+    else:
+        return globals.client.start(app_token, reconnect=True)
+
+
+if __name__ == "__main__":
+    start_client()
