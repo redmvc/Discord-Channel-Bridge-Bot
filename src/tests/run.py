00afcd9bb40a727087dcf6ee09f21090465c25ba
Red Magnos <redmagnos@gmail.com>
2025-07-05 14:45:37 -0300

    Create basic skeleton of test runner script

---
+++ src/tests/run.py
@@ -0,0 +1,44 @@
+import asyncio
+import sys
+from pathlib import Path
+import tester_bot
+from tester_bot import start_client as start_tester_bot
+
+sys.path.append(str(Path(__file__).parent.parent))
+import globals
+from main import start_client as start_bridge_bot
+
+
+class Bots:
+    async def __aenter__(self):
+        self.running_bridge_bot_client_task = asyncio.create_task(
+            start_bridge_bot(False)
+        )
+        self.bridge_bot_client = globals.client
+
+        self.running_tester_bot_client_task = asyncio.create_task(start_tester_bot())
+        self.tester_bot_client = tester_bot.client
+
+        await asyncio.gather(globals.wait_until_ready(), tester_bot.wait_until_ready())
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await asyncio.gather(
+            asyncio.create_task(self.bridge_bot_client.close()),
+            self.running_bridge_bot_client_task,
+            asyncio.create_task(self.tester_bot_client.close()),
+            self.running_tester_bot_client_task,
+        )
+
+        return True
+
+
+async def run_tests():
+    async with Bots() as bots:
+        print("things")
+        await asyncio.sleep(10)
+        print("stuff")
+
+
+if __name__ == "__main__":
+    asyncio.run(run_tests())
